d47d8d0b00a5fbdfd21ed7b277e89ab7cf04c451
Abhijith S <abheaks.007@gmail.com>
2023-07-18 19:06:03 +0530

    react form hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import StarRating from "./components/StarRating";
 import Welcome from "./components/Welcome";
+import FormHook from "./components/formHook";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/star" element={<StarRating />} />
+        <Route path="/formHook" element={<FormHook />} />
         <Route path="*" element={<Welcome />} />
       </Routes>
     </>
